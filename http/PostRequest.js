a036a0c11817d2c7e5bea1420709fd76fca70f2a
Jonathan Havens <70242056+havens-dev@users.noreply.github.com>
2020-09-08 13:53:40 -0700

    Update PostRequest.js

--- http/PostRequest.js
+++ http/PostRequest.js
@@ -1,5 +1,59 @@
 //TODO - Add 'PostParams' class that parses each parameter when built
 
+/**
+BASIC USAGE
+--
+
+=> /post to prod url
+https://webappurl.com/exec?query_item_one=foo&query_item_two=bar
+
+--
+(postBody)
+
+  {
+	  "body_one_firstname": "john",
+	  "body_two_lastname": "doe"
+  }
+)
+-- 
+
+function doPost(e) {
+  return new PostRequest(e).jsonResponse()
+}
+
+--
+
+returns ...
+{
+    "status": 200,
+    "data": {
+        "queryString": "query_item_one=foo&query_item_two=bar",
+        "contentLength": 62,
+        "queryParams": {
+            "query_item_one": "foo",
+            "query_item_two": "bar"
+        },
+        "payload": {
+            "contents": {
+                "body_one_firstname": "john",
+                "body_two_lastname": "doe"
+            },
+            "length": 62,
+            "name": "postData",
+            "type": "application/json"
+        },
+        "parameters": {
+            "query_item_two": [
+                "bar"
+            ],
+            "query_item_one": [
+                "foo"
+            ]
+        }
+    }
+}
+*/
+
 class PostRequest {
 
   constructor(event) {
